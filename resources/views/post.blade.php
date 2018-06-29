4440ba800dac7fc56e5ec24ef78007538b2f5a41
Karlo Tomasic <karlo.tomasic@outlook.com>
2018-06-29 22:31:19 +0200

    Show Posts on Home Page

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,14 +1,14 @@
 @extends('Centaur::layout')
 
-@section('title') 
+@section('title')
 Algebra Blog | {{ $post->title }}
 @endsection
 
 @section('content')
 	<div class="page-header">
-        <h1>{{ $post->title }}</h1>
-		<small>Author: {{ $post->user->email }}</small><br>
-		<small>Published: {{ \Carbon\Carbon::createFromTimestamp(strtotime($post->created_at))->diffForHumans() }}</small>
+        <h1>{{ $post->title }}</h1> //ispisuje se naslov posta
+		<small>Author: {{ $post->user->email }}</small><br> //ispis autora
+		<small>Published: {{ \Carbon\Carbon::createFromTimestamp(strtotime($post->created_at))->diffForHumans() }}</small> //ispis datuma kreiranja
     </div>
 	<div class="row">
 		<div class="col-sm-12">
